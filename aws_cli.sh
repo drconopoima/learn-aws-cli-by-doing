cc74a753376cfa4346c0d95750218d12237be927
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2022-02-21 21:40:23 +0100

    S3 lifecycle policy trans. Glacier & DEEP_ARCHIVE

--- aws_cli.sh
+++ aws_cli.sh
@@ -27,6 +27,7 @@ set -x
 aws --region="${AWS_DEFAULT_REGION}" s3api create-bucket --bucket "${S3_BUCKET_NAME}" --acl public-read --output text | cat
 # ERROR: 'An error occurred (IllegalLocationConstraintException) when calling the CreateBucket operation: The unspecified location constraint is incompatible for the region specific endpoint this request was sent to.'
 # SOLUTION: Add specification flag --region
+# Your user may have permissions to access at a specific region only
 # + aws s3api create-bucket --bucket dtfgrg734a4 --acl public-read --output text
 # /dtfgrg734a4
 { set +x; } 2>/dev/null
@@ -36,8 +37,8 @@ aws --region="${AWS_DEFAULT_REGION}" s3api get-public-access-block --bucket "${S
 # ERROR: An error occurred (NoSuchPublicAccessBlockConfiguration) when calling the GetPublicAccessBlock operation: The public access block configuration was not found
 # SOLUTION: Execute the following command to set public-access-block for bucket
 aws --region="${AWS_DEFAULT_REGION}" s3api put-public-access-block \
-    --bucket "${S3_BUCKET_NAME}" \
-    --public-access-block-configuration "BlockPublicAcls=false,IgnorePublicAcls=false,BlockPublicPolicy=false,RestrictPublicBuckets=false"
+--bucket "${S3_BUCKET_NAME}" \
+--public-access-block-configuration "BlockPublicAcls=false,IgnorePublicAcls=false,BlockPublicPolicy=false,RestrictPublicBuckets=false"
 # SILENT SUCCESS: No output given
 # public-access-block documentation: https://stackoverflow.com/questions/63389666/block-all-objects-public-access-in-s3-by-cli
 aws --region="${AWS_DEFAULT_REGION}" s3api get-public-access-block --bucket "${S3_BUCKET_NAME}" --output json | jq
@@ -49,3 +50,46 @@ aws --region="${AWS_DEFAULT_REGION}" s3api get-public-access-block --bucket "${S
 #     "RestrictPublicBuckets": false
 #   }
 # }
+##### Upload a folder's contents
+# clone project folder
+git clone https://github.com/tia-la/ccp.git
+cd ccp || exit
+aws s3 sync ./ "s3://${S3_BUCKET_NAME}"
+##### Create lifecycle transition to Glacier S3 objects of prefix pinehead
+cat<<EOF>pinehead_lifecycle.json
+{
+    "Rules": [
+        {
+            "ID": "sample-s3-to-glacier-rule",
+            "Filter": {
+                "Prefix": "pinehead"
+            },
+            "Status": "Enabled",
+            "Transitions": [
+                {
+                    "Days": 30,
+                    "StorageClass": "GLACIER"
+                }
+            ],
+            "NoncurrentVersionTransitions": [
+                {
+                    "NoncurrentDays": 15,
+                    "StorageClass": "DEEP_ARCHIVE",
+                    "NewerNoncurrentVersions": 1
+                }
+            ],
+            "Expiration": {
+                    "Days": 730
+            }
+        }
+    ]
+}
+EOF
+aws s3api put-bucket-lifecycle-configuration --bucket "${S3_BUCKET_NAME}" --lifecycle-configuration file://pinehead_lifecycle.json
+# SILENT SUCCESS: No output given
+aws s3api get-bucket-lifecycle-configuration --bucket "${S3_BUCKET_NAME}" | cat
+# RULES   sample-s3-to-glacier-rule   Enabled
+# EXPIRATION  730
+# FILTER  pinehead
+# NONCURRENTVERSIONTRANSITIONS    1   15  DEEP_ARCHIVE
+# TRANSITIONS 30  GLACIER
